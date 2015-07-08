c11602de45f919d729bd207289c3ba91618fd03b
methodcase <methodcase@gmail.com>
2015-07-08 23:33:48 +0800

    added IIFE

--- RH.js
+++ RH.js
@@ -1,41 +1,44 @@
-'use strict';
-var RH = {
-    get: function (url, callback) {
-        var xmlhttp;
-        if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {// code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                callback(xmlhttp.responseText);
+(function(){
+    'use strict';    
+    var RH = {
+        get: function (url, callback) {
+            var xmlhttp;
+            if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            } else {// code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
             }
-        };
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
-    },
-    post: function (url, json_data, callback) {
-        var xmlhttp;
-        if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {// code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                callback(xmlhttp.responseText);
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                    callback(xmlhttp.responseText);
+                }
+            };
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+        },
+        post: function (url, json_data, callback) {
+            var xmlhttp;
+            if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            } else {// code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
             }
-        };
-        xmlhttp.open("POST", url, true);
-        xmlhttp.send(this.serialize_json(json_data));
-    },
-    serialize_json: function (json_str) {
-        var s = "";
-        var b = JSON.parse(JSON.stringify(json_str));
-        for (var x in b){
-            s += x + "=" +b[x]+"&"
-        }
-        return s.substr(0,s.length - 1);
-     }	
-};	
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                    callback(xmlhttp.responseText);
+                }
+            };
+            xmlhttp.open("POST", url, true);
+            xmlhttp.send(this.serialize_json(json_data));
+        },
+        serialize_json: function (json_str) {
+            var s = "";
+            var b = JSON.parse(JSON.stringify(json_str));
+            for (var x in b){
+                s += x + "=" +b[x]+"&"
+            }
+            return s.substr(0,s.length - 1);
+        }   
+    };   
+    self.RH = RH; 
+})();
